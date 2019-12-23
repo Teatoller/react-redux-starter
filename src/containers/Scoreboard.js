d06fbd8b9cb20669de39eed395a74b217d64f102
Teatoller <steven.otieno1@gmail.com>
2019-12-23 12:15:41 +0300

    (bindactioncreators): Implement bound action creators
    - import bindaction creator
    - import action creators
    - destructure dispatch and players
    - bind the actions
    - map over players array
    - refactor props in header
    - replace the player component with playerComponents
    - refactor props in AddPlayer component
    
    [Starts #008]

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import * as PlayerActionCreators from "../actions/PlayerActions";
 
 import Player from "../components/Player";
 import Header from "../components/Header";
@@ -13,25 +15,39 @@ class Scoreboard extends Component {
   };
 
   render() {
+    console.log("....this.props", this.props);
+
+    const { dispatch, players } = this.props;
+
+    const addPlayer = bindActionCreators(
+      PlayerActionCreators.addPlayer,
+      dispatch
+    );
+    const removePlayer = bindActionCreators(
+      PlayerActionCreators.removePlayer,
+      dispatch
+    );
+    const updatePlayerScore = bindActionCreators(
+      PlayerActionCreators.updatePlayerScore,
+      dispatch
+    );
+
+    const playerComponents = players.map((player, index) => (
+      <Player
+        index={index}
+        name={player.name}
+        score={player.score}
+        key={player.name}
+        updatePlayerScore={updatePlayerScore}
+        removePlayer={removePlayer}
+      />
+    ));
+
     return (
       <div className="scoreboard">
-        <Header players={this.state.players} />
-        <div className="players">
-          {this.state.players.map(
-            function(player, index) {
-              return (
-                <Player
-                  name={player.name}
-                  score={player.score}
-                  key={player.name}
-                  onScoreChange={delta => this.onScoreChange(index, delta)}
-                  onRemove={() => this.onRemovePlayer(index)}
-                />
-              );
-            }.bind(this)
-          )}
-        </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
+        <Header players={players} />
+        <div className="players">{playerComponents}</div>
+        <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
   }
